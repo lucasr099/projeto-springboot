f9ffe25b363d46208f27bf18eedbb2dd5221511f
Lucas Costa <lucasreiiss100@hotmail.com>
2025-11-15 10:19:31 -0300

    service layes e componet registracion

--- src/main/java/com/lucasreis/curso/repositories/UserRepository.java
+++ src/main/java/com/lucasreis/curso/repositories/UserRepository.java
@@ -3,5 +3,7 @@ package com.lucasreis.curso.repositories;
 import com.lucasreis.curso.entites.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
