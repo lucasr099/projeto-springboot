5f0aafc8727b4c617c2a2755090dc0f57b0c9dd0
Lucas Costa <lucasreiiss100@hotmail.com>
2025-11-17 17:32:27 -0300

    exception update

--- src/main/java/com/lucasreis/curso/services/UserServices.java
+++ src/main/java/com/lucasreis/curso/services/UserServices.java
@@ -4,6 +4,7 @@ import com.lucasreis.curso.entites.User;
 import com.lucasreis.curso.repositories.UserRepository;
 import com.lucasreis.curso.services.exception.DatabaseException;
 import com.lucasreis.curso.services.exception.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,13 @@ public class UserServices {
         }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
